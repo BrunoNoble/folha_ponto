1beffbb5528839f22f6f2dd592a369d31895b9fb
BrunoNoble <brunonoblestrategy@gmail.com>
2023-11-22 17:45:31 +0000

    v 1.1 verificar  mes horas / total, registro v 0.1

---
+++ resources/views/register-ponto.blade.php
@@ -0,0 +1,11 @@
+@vite(['resources/sass/app.scss', 'resources/js/app.js'])
+
+<div class="container">
+    <form action="" method="get">
+
+        <input value="{{\Carbon\Carbon::now()}}" name="time" type="hidden">
+        <button type="submit" class="btn btn-success mt-3">Teste</button>
+
+    </form>
+
+</div>
